4bf56471c1af6c61d13958bf9aebd71803c869d1
Anthony (stranger) <luttrell321@gmail.com>
2020-02-05 06:39:13 -0600

    Added do-while loop to goToMainMenu and changed user input to Integer.parseInt(reader.nextLine())

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main {
     private static Scanner reader = new Scanner(System.in);
     private static Menu main = new Menu();
-    private static int completed = 0;
 
     public static void main(String[] args) throws IOException {
         goToMainMenu();
@@ -26,43 +25,49 @@ public class Main {
                 Menu.setOptionNames("grams", "ounces", "volume", "exit"), Menu.setOptionNumbers('1', '2', '3', '0'),
                 Menu.width);
 
-        //FIXME was unable to add a do-while loop around the try-catch block below without it creating an infinite loop if you enter any non-int for userMainChoice
-
-        try {
-            System.out.print("Enter your choice here: ");
-            int userMainChoice = reader.nextInt();
-            System.out.println();
-            while (userMainChoice < 0 || userMainChoice > 3) {
-                System.out.print("You must enter a valid option, try again: ");
-                userMainChoice = reader.nextInt();
-                System.out.println();
-            }
-            if (userMainChoice == 0) {
-                System.out.println("Goodbye!");
-            }
-            if (userMainChoice == 1) {
-                Grams.gotoGramsMenu();
-            } else if (userMainChoice == 2) {
-                Ounces.gotoOuncesMenu();
-            } else if (userMainChoice == 3) {
-                Menu.drawMenu(main.charsTitle, main.charsBanner, main.charsRow, Menu.TITLE, "Proceed?", Menu.setOptionNames("proceed", "go back"),
-                        Menu.setOptionNumbers('1', '0'), Menu.width);
-                System.out.println("Using volume measurements is not recommended, you will experience \ninherent rounding errors.");
-                System.out.println("Are you sure you wish to proceed?");
-                System.out.println();
+        boolean correctInput = false;
+        do {
+            try {
                 System.out.print("Enter your choice here: ");
-                int proceed = reader.nextInt();
-                if (proceed == 1) {
-                    Volume.gotoVolumeMenu();
-                } else {
-                    goToMainMenu();
+                int userMainChoice = Integer.parseInt(reader.nextLine());
+                System.out.println();
+                while (userMainChoice < 0 || userMainChoice > 3) {
+                    System.out.print("You must enter a valid option, try again: ");
+                    userMainChoice = Integer.parseInt(reader.nextLine());
+                    System.out.println();
                 }
+                if (userMainChoice == 0) {
+                    System.out.println("Goodbye!");
+                }
+                if (userMainChoice == 1) {
+                    correctInput = true;
+                    Grams.gotoGramsMenu();
+                } else if (userMainChoice == 2) {
+                    correctInput = true;
+                    Ounces.gotoOuncesMenu();
+                } else if (userMainChoice == 3) {
+                    correctInput = true;
+                    Menu.drawMenu(main.charsTitle, main.charsBanner, main.charsRow, Menu.TITLE, "Proceed?", Menu.setOptionNames("proceed", "go back"),
+                            Menu.setOptionNumbers('1', '0'), Menu.width);
+                    System.out.println("Using volume measurements is not recommended, you will experience \ninherent rounding errors.");
+                    System.out.println("Are you sure you wish to proceed?");
+                    System.out.println();
+                    System.out.print("Enter your choice here: ");
+                    int proceed = reader.nextInt();
+                    if (proceed == 1) {
+                        Volume.gotoVolumeMenu();
+                    } else {
+                        goToMainMenu();
+                    }
+                    correctInput = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println();
+                System.out.println("You must enter a valid number!");
+                System.out.println();
             }
-        } catch (InputMismatchException e) {
-            System.out.println();
-            System.out.println("You must enter a valid number!");
-            System.out.println();
         }
+        while (!correctInput);
     }
 }
 
